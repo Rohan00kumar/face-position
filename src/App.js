6fdf399d2f312450792de0a732d3a7c9227f76b7
Rohan <113842997+Rohan00kumar@users.noreply.github.com>
2024-02-26 14:06:19 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import React, { Fragment } from "react";
+import WebcamImg from "./components/webcamImg/WebcamImg";
+
+const App = () => {
+  return (
+    <Fragment>
+      <main>
+        <WebcamImg />
+      </main>
+    </Fragment>
+  );
+};
+
+export default App;
